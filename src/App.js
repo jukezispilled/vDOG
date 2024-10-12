2a8c8ce720b95ec33ec411da729de7eed29e05c5
Jack Anderson <whotfisjax@gmail.com>
2024-10-11 21:50:58 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
               <div className="flex space-x-3">
                 <h1 className="text-xl font-extrabold md:text-5xl">vDOG</h1>
                 <div className='flex justify-center items-center z-10'>
-                  <a href="https://x.com/vDOGsolana" className=''>
+                  <a href="https://x.com/vDOGtoken" className=''>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-4 md:size-6 md:hover:scale-105 transition ease-in-out duration-150' fill="#00000" viewBox="0 0 50 50">
                       <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
                     </svg>
