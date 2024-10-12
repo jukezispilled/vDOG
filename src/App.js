9a2ca27391e49f78505b7b3a69385ffa27c48bc8
Jack Anderson <whotfisjax@gmail.com>
2024-10-11 21:30:19 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -12,31 +12,31 @@ export default function App() {
       <div className="max-w-6xl mx-auto px-4 py-4 md:py-8 w-[95%] bg-zinc-200">
         <header className="grid">
           <div className="w-full flex justify-center">
-            <img src="he.png" className="mb-6 rounded-xl w-[100%]"></img>
+            <img src="vh.png" className="mb-6 rounded-xl w-[100%]"></img>
           </div>
           <div className="flex items-center gap-4 mb-6">
             <div className="w-48 md:size-40 relative overflow-hidden rounded-full">
-              <img src="po.png" alt="User" className="w-full h-full" />
+              <img src="v.png" alt="User" className="w-full h-full" />
             </div>
             <div className="md:space-y-0 md:-mt-1">
               <div className="flex space-x-3">
-                <h1 className="text-xl font-extrabold md:text-5xl">POV</h1>
+                <h1 className="text-xl font-extrabold md:text-5xl">vDOG</h1>
                 <div className='flex justify-center items-center z-10'>
-                  <a href="https://x.com/povmonk" className=''>
+                  <a href="https://x.com/" className=''>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-4 md:size-6 md:hover:scale-105 transition ease-in-out duration-150' fill="#00000" viewBox="0 0 50 50">
                       <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
                     </svg>
                   </a>
-                  <a href="https://t.me/povportalsolanaa" className=''>
+                  <a href="https://t.me/" className=''>
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-4 md:size-6 md:hover:scale-105 transition ease-in-out duration-150' fill="#29A0DA" viewBox="0 0 50 50">
                       <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
                     </svg>
                   </a>
                 </div>
               </div>
-              <p className="text-sm md:text-base text-gray-600">@POV • 599K subscribers</p>
-              <p className="text-xs md:text-base text-gray-600">28 years old. Entreprenuer. Sharing my POV on things.</p>
-              <p className="text-xs md:text-base text-gray-600 pb-1">CA: 2fg1B7hSJF2EQ3sY7bZhZ7hD3HztJtR4boiYQ4EBpump</p>
+              <p className="text-sm md:text-base text-gray-600">@vDOG • 420K subscribers</p>
+              <p className="text-xs md:text-base text-gray-600">28 years old. Entreprenuer. Sharing my journey.</p>
+              <p className="text-xs md:text-base text-gray-600 pb-1">CA: soon...</p>
               <button 
                 className={`py-2 px-3 text-base md:text-lg rounded-full text-white font-semibold transition-colors duration-300 ${
                   isSubscribed ? 'bg-gray-500 hover:bg-gray-600' : 'bg-red-600 hover:bg-red-700'
@@ -50,68 +50,68 @@ export default function App() {
         </header>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
           <VideoCard 
-            title="10 Investing Tips from Warren Buffet" 
+            title="Tesla Event | Optimus" 
             views="1.2M" 
             time="2 days ago" 
             likes="1.2K" 
             dislikes="100"
-            image="/x1.png" 
+            image="/y3.png" 
           />
           <VideoCard 
-            title="What it's like Flying Private Jets" 
+            title="Courtside @ Lakers" 
             views="1.5M" 
             time="5 days ago" 
             likes="500" 
             dislikes="20"
-            image="/x7.png" 
+            image="/y2.png" 
           />
           <VideoCard 
-            title="Living 24hrs like Jordan Belfort" 
+            title="Helene Aftermath" 
             views="12M" 
             time="10 days ago" 
             likes="979" 
             dislikes="10"
-            image="/x.png" 
+            image="/y7.png" 
           />
           <VideoCard 
-            title="HISTORY MADE @ UFC 304" 
+            title="10 Investing Tips from Warren Buffet" 
             views="7M" 
             time="3 week ago" 
             likes="1.5K" 
             dislikes="50"
-            image="/x3.png" 
+            image="/y.png" 
           />
           <VideoCard 
-            title="Lakers Game Courtside" 
+            title="Cigar Talk w/ Tates!" 
             views="1.8M" 
             time="3 weeks ago" 
             likes="2.5K" 
             dislikes="200"
-            image="/x6.png" 
+            image="/y1.png" 
           />
           <VideoCard 
-            title="Cigar Talk w/ Tates!" 
+            title="Living 24hrs like Jordan Belfort" 
             views="3M" 
             time="1 month ago" 
             likes="1.5K" 
             dislikes="50"
-            image="/x4.png" 
+            image="/y4.png" 
           />
           <VideoCard 
-            title="Penthouse Tour" 
+            title="What it's like Flying Private Jets" 
             views="2M" 
             time="2 month ago" 
             likes="1.8K" 
             dislikes="30"
-            image="/x5.png" 
+            image="/y5.png" 
           />
           <VideoCard 
-            title="All White Recap" 
+            title="Penthouse Tour" 
             views="1.1M" 
             time="4 month ago" 
             likes="1.6K" 
             dislikes="57"
-            image="/x2.png" 
+            image="/y6.png" 
           />
         </div>
       </div>
